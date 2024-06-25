e47dcf034ebd907483dba8e549baec78b2c617a5
Shieldine <74987363+Shieldine@users.noreply.github.com>
2024-06-25 22:01:28 +0200

    :memo: update readme and root description

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # git-profile
-Simple git plugin to manage and automatically set git user profiles based on the project's origin
+Simple git plugin to manage and automatically set git user profiles based on the project's origin.
+
+Okay, but why?<br />
+Some developers use their computers for both work-related and private projects.
+This usually involves having *at least* two different sets of credentials
+for git. If your private project and/or work involve multiple platforms to keep
+your source-code on, that increases the number of credential sets you need to manage.
+Setting those each time you clone a project can be quite tiresome and - if you forget or misspell something -
+lead to the need of amending commits.
+
+With git-profile, you need to type in your attributes exactly *once*.
+They get saved in a *profile* along with the project's origin. Upon calling git-profile
+in a repository, it will automatically pick a profile based on the origin
+and simply set those attributes for you - no need to even remember a profile name!
+
+At the same time, you still get a few little extra commands to have manual control
+over your attributes.
+
+
+## Installation
+
+
+## Getting started
+
+
+## Development
--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 Shieldine <EMAIL ADDRESS>
+Copyright © 2024 Shieldine <74987363+Shieldine@users.noreply.github.com>
 */
 package cmd
 
@@ -9,19 +9,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "git-profile",
 	Short: "Manage and automatically set git user profiles based on the project's origin",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Long: `git-profile is a simple CLI to manage and automatically set git user profiles based on the project's origin.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+Save a profile together with its origin and let git-profile set the attributes next time you clone a new repository.
+To make managing names and emails more convenient in general, git-profile offers further commands that will let you
+check, unset and set credentials without creating a profile.
+`,
 }
 
 func Execute() {
@@ -32,13 +28,4 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "internal", "", "internal file (default is $HOME/.git-profile.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
